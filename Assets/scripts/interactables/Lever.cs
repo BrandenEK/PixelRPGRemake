79581e0b4ac1d3d0bccab9309e646389766f37ad
Branden Wood <brandenek03912@hotmail.com>
2023-12-02 10:47:04 -0500

    world: more functionality for levers

--- Assets/scripts/interactables/Lever.cs
+++ Assets/scripts/interactables/Lever.cs
@@ -1,24 +1,46 @@
+using PixelRPG.Persistence;
 using UnityEngine;
 
 namespace PixelRPG.Interactables
 {
-    public class Lever : MonoBehaviour, IInteractable
+    public class Lever : MonoBehaviour, IInteractable, IPersistentObject
     {
+        public void Interact()
+        {
+            Debug.Log("Toggling lever");
+            anim.SetTrigger("triggering");
+            _active = !_active;
+        }
+
+        public Vector3 PopupPosition => transform.position + Vector3.up * 0.8f;
+
+        public bool IsInteractable => !_active || !_singleUse;
+
+        public bool CurrentStatus
+        {
+            get => _active;
+            set
+            {
+                if (!_persistState)
+                    return;
+
+                _active = value;
+                anim.SetTrigger(value ? "right" : "left");
+            }
+        }
+
+        public int SceneIndex => _sceneIndex;
+
+        [SerializeField] int _sceneIndex;
+        [SerializeField] bool _persistState;
+        [SerializeField] bool _singleUse;
+        private bool _active;
+
         private Animator anim;
 
         private void Awake()
         {
             anim = GetComponentInChildren<Animator>();
         }
-
-        public void Interact()
-        {
-            Debug.Log("Toggling lever");
-            anim.SetTrigger("triggering");
-        }
-
-        public Vector3 PopupPosition => transform.position + Vector3.up * 0.8f;
-
-        public bool IsInteractable => true;
     }
 }
